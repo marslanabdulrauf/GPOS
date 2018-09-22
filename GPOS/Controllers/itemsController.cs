bcf0459a1ada7a3c869d30a9cc45aed8de489439
Muhammad Arslan <m.arslanabdulrauf@gmail.com>
2018-09-22 17:29:26 +0500

    .

--- GPOS/Controllers/itemsController.cs
+++ GPOS/Controllers/itemsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Newtonsoft.Json;
 using GPOS.Models;
 using GPOSDAL;
 using Kendo.Mvc.Extensions;
